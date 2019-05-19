7443d243229727a225efd101461df66bdc1d68cb
eduardogharcia <eduardogarcia.nv@gmail.com>
2019-05-19 00:40:01 -0300

    GH Pages deploy script

--- public/lines.js
+++ public/lines.js
@@ -1,39 +1,133 @@
 [
   {
-    "id": "catalao-goiandira",
-    "from": "Catalão",
-    "to": "Goiandira",
+    "id": "goiandira-catalao",
+    "from": "Goiandira",
+    "to": "Catalão",
     "schedule": [
+      {
+        "time": "4:50",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "6:10",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "6:20",
+        "company": "Transduarte"
+      },
+      {
+        "time": "7:00",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "7:30",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "7:30",
+        "company": "Transduarte"
+      },
+      {
+        "time": "7:40",
+        "company": "Expresso Araguari"
+      },
       {
         "time": "9:20",
         "company": "Expresso Araguari"
       },
       {
-        "time": "10:20",
-        "company": "Expresso Araguari",
-        "obs": "seg. a sex."
+        "time": "9:20",
+        "company": "Transduarte"
       },
       {
-        "time": "11:20",
-        "company": "Expresso Araguari",
-        "obs": ""
+        "time": "11:15",
+        "company": "Expresso Araguari"
       },
       {
-        "time": "13:20",
-        "company": "Expresso Araguari",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "13:20",
+        "time": "11:15",
         "company": "Transduarte",
         "obs": "seg. a sex."
+      },
+      {
+        "time": "12:00",
+        "company": "Transduarte"
+      },
+      {
+        "time": "12:40",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "13:30",
+        "company": "Transduarte",
+        "obs": "seg. a sex."
+      },
+      {
+        "time": "14:20",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "15:30",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "16:05",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "16:15",
+        "company": "Transduarte",
+        "obs": "seg. a sex."
+      },
+      {
+        "time": "17:15",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "17:20",
+        "company": "Transduarte",
+        "obs": "seg. a sex."
+      },
+      {
+        "time": "18:00",
+        "company": "Transduarte",
+        "obs": "sábado"
+      },
+      {
+        "time": "18:20",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "18:20",
+        "company": "Transduarte",
+        "obs": "seg. a sex."
+      },
+      {
+        "time": "19:20",
+        "company": "Transduarte"
+      },
+      {
+        "time": "20:10",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "20:40",
+        "company": "Expresso Araguari"
+      },
+      {
+        "time": "20:45",
+        "company": "Transduarte"
+      },
+      {
+        "time": "21:40",
+        "company": "Expresso Araguari"
       }
     ]
   },
   {
-    "id": "goiandira-catalao",
-    "from": "Goiandira",
-    "to": "Catalão",
+    "id": "catalao-goiandira",
+    "from": "Catalão",
+    "to": "Goiandira",
     "schedule": [
       {
         "time": "9:20",
