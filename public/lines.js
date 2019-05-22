75ba7a87d3e6dcb637027e088dd15319f72d7f56
eduardogharcia <eduardogarcia.nv@gmail.com>
2019-05-22 17:36:37 -0300

    The right schedule for Transduarte's catalão > goiandira line

--- public/lines.js
+++ public/lines.js
@@ -5,122 +5,9 @@
     "to": "Catalão",
     "schedule": [
       {
-        "time": "4:50",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "6:10",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "6:20",
-        "company": "Transduarte"
-      },
-      {
-        "time": "7:00",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "7:30",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "7:30",
-        "company": "Transduarte"
-      },
-      {
-        "time": "7:40",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "9:20",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "9:20",
-        "company": "Transduarte"
-      },
-      {
-        "time": "11:15",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "11:15",
-        "company": "Transduarte",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "12:00",
-        "company": "Transduarte"
-      },
-      {
-        "time": "12:40",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "13:30",
-        "company": "Transduarte",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "14:20",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "15:30",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "16:05",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "16:15",
-        "company": "Transduarte",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "17:15",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "17:20",
-        "company": "Transduarte",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "18:00",
-        "company": "Transduarte",
-        "obs": "sábado"
-      },
-      {
-        "time": "18:20",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "18:20",
-        "company": "Transduarte",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "19:20",
-        "company": "Transduarte"
-      },
-      {
-        "time": "20:10",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "20:40",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "20:45",
-        "company": "Transduarte"
-      },
-      {
-        "time": "21:40",
-        "company": "Expresso Araguari"
+        "time": "0:00",
+        "company": "************",
+        "obs": "Horários pendentes"
       }
     ]
   },
@@ -130,28 +17,69 @@
     "to": "Goiandira",
     "schedule": [
       {
-        "time": "9:20",
-        "company": "Expresso Araguari"
-      },
-      {
-        "time": "10:20",
-        "company": "Expresso Araguari",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "11:20",
-        "company": "Expresso Araguari",
-        "obs": ""
-      },
-      {
-        "time": "13:20",
-        "company": "Expresso Araguari",
-        "obs": "seg. a sex."
-      },
-      {
-        "time": "13:20",
+        "time": "6:20",
         "company": "Transduarte",
-        "obs": "seg. a sex."
+        "obs": "Somente domingos"
+      },
+      {
+        "time": "7:00",
+        "company": "Transduarte",
+        "obs": "Exceto domingos"
+      },
+      {
+        "time": "9:00",
+        "company": "Transduarte",
+        "obs": "Exceto domingos"
+      },
+      {
+        "time": "11:00",
+        "company": "Transduarte",
+        "obs": "Exceto domingos"
+      },
+      {
+        "time": "12:00",
+        "company": "Transduarte",
+        "obs": "Exceto sábados e feriados"
+      },
+      {
+        "time": "12:30",
+        "company": "Transduarte",
+        "obs": "Sábados e feriados"
+      },
+      {
+        "time": "13:00",
+        "company": "Transduarte",
+        "obs": "Seg. a sex."
+      },
+      {
+        "time": "15:30",
+        "company": "Transduarte",
+        "obs": "Seg. a sex."
+      },
+      {
+        "time": "17:00",
+        "company": "Transduarte",
+        "obs": "Seg. a sex."
+      },
+      {
+        "time": "18:00",
+        "company": "Transduarte",
+        "obs": "Exceto sábados e feriados"
+      },
+      {
+        "time": "18:30",
+        "company": "Transduarte",
+        "obs": "sábados e feriados"
+      },
+      {
+        "time": "19:00",
+        "company": "Transduarte",
+        "obs": "Seg. a sex."
+      },
+      {
+        "time": "21:00",
+        "company": "Transduarte",
+        "obs": "Seg. a sex."
       }
     ]
   }
