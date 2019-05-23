ed91e4d09696cd8f0e7df86917a545a70227c100
eduardogharcia <eduardogarcia.nv@gmail.com>
2019-05-23 17:38:00 -0300

    Small spelling mistake

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
   <aside class="about">
     <h2>Olá! ;)</h2>
     <p>Meu nome é Eduardo Garcia e criei esse app para facilitar a vida de quem precisa pegar busão.</p>
-    <p>Horários incoretos? sugira mudanças nos contatos abaixo.</p>
+    <p>Horários incorretos? sugira mudanças nos contatos abaixo.</p>
     <p>Trabalho com <b>sites</b> e <b>sistemas</b>, se precisar é só chamar no <a href="mailto:eduardogarcia.nv@gmail.com">eduardogarcia.nv@gmail.com</a></p>
     <p><a target="_blank" href="https://instagram.com/eduardogharcia">@eduardogharcia</a></p>
     <p><small>*Não nos responsabilizamos por horários incorretos, as empresas podem alterá-los sem aviso prévio</small></p>
