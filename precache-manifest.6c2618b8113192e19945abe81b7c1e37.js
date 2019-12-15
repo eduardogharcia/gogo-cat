04b69c24347d18101aa67b65be3e3674dc2d69bf
eduardogharcia <eduardogarcia.nv@gmail.com>
2019-12-15 19:35:46 -0300

    29662b9

---
+++ precache-manifest.6c2618b8113192e19945abe81b7c1e37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c8bc3e5e835ab3b091d2",
+    "url": "/js/chunk-vendors.52a4f118.js"
+  },
+  {
+    "revision": "08bc6109992c946bf9b6",
+    "url": "/js/app.33f7a685.js"
+  },
+  {
+    "revision": "0406bc66102634d043acb06c2f91395d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bc6109992c946bf9b6",
+    "url": "/css/app.2ba06195.css"
+  }
+];
